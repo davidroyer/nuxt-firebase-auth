8170ddb955e92d9e98e02230a4c3aee7691ece3c
David Royer <droyer01@gmail.com>
2017-10-12 05:01:36 -0400

    updated heading

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-flex text-xs-center xs12 sm6 offset-sm3>
-      <h5>Sign In with Google</h5>
+      <h2 class="title">Sign In with Google</h2>
      <v-btn class="signIn mb-2" primary @click.native="googleSignUp">Google Sign In</v-btn>
     </v-flex>
   </v-layout>
