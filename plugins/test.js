5781ad15a08d0afe7c77aad26b6c43644eeca213
David Royer <droyer01@gmail.com>
2017-11-02 20:03:08 -0400

    save

--- plugins/test.js
+++ plugins/test.js
@@ -1,41 +1,11 @@
 import firebase from '~/plugins/firebase-client-init.js'
 
 export default (context) => {
-  const {store, app, redirect} = context
-  console.log('FROM TEST: RUNNING');
+  const {store} = context
 
   firebase.auth().onAuthStateChanged(function (user) {
     if (user && !store.getters.activeUser) {
-      console.log('FROM TEST: ', user);
       store.commit('setUser', user)
     }
-
-    // resolve(user)
   })
-  // return new Promise(function (resolve, reject) {
-  //   firebase.auth().onAuthStateChanged(function (user) {
-  //     console.log('FROM TEST: ', user);
-  //     store.commit('setUser', user)
-  //     resolve(user)
-  //   })
-  // })
-
 }
-
-
-
-  // const getUserStatus = function (store) {
-  //   // store.dispatch('CHECK_USER_STATUS');
-  //
-  //   return new Promise(function (resolve, reject) {
-  //     firebaseApp.auth().onAuthStateChanged(function (user) {
-  //       if (user) {
-  //         store.dispatch('LOGIN_SUCCESS', user.uid);
-  //         resolve(user);
-  //       } else {
-  //         store.dispatch('LOGIN_FAIL');
-  //         reject(Error('It broke'));
-  //       }
-  //     });
-  //   });
-  // };
