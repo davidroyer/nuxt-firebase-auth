1ca36bc4148176577170f9d1802f66e5a718bd6b
David Royer <droyer01@gmail.com>
2018-12-14 09:36:35 -0500

    removed unused code

--- static/sw.js
+++ static/sw.js
@@ -1,64 +1,73 @@
-importScripts('/_nuxt/workbox.476439e0.js')
+importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
-const workboxSW = new self.WorkboxSW({
-  "cacheId": "nuxt-firebase-auth",
-  "clientsClaim": true,
-  "directoryIndex": "/"
-})
 
-workboxSW.precache([
+
+workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/app.c6633c28a44a3c87cb5583240f546dec.css",
-    "revision": "c6633c28a44a3c87cb5583240f546dec"
+    "url": "/_nuxt/app.958c25d3c5b9d1c6c5b3.js",
+    "revision": "302d83c62f7d75e4bcb82709755c822d"
   },
   {
-    "url": "/_nuxt/app.f2d08e5aa55180199f35.js",
-    "revision": "650a2b21a948b70a73e8c20a386b05e0"
+    "url": "/_nuxt/app.befb363a830a9ba56dba600f655c15ce.css",
+    "revision": "befb363a830a9ba56dba600f655c15ce"
   },
   {
-    "url": "/_nuxt/layouts/default.7aaeee9d41fb311d763c.js",
-    "revision": "34c184ebf76297a9e323a1813698f807"
+    "url": "/_nuxt/layouts/default.ed07cbfe31cb8f640461.js",
+    "revision": "ce92ce6d82832bce379ad0ccdd726dd8"
   },
   {
-    "url": "/_nuxt/manifest.77406ca4d6b2c1d8cf04.js",
-    "revision": "f0c91ff4a59d493f6b5a073725f0222c"
+    "url": "/_nuxt/manifest.2b145708a9fe977a75c5.js",
+    "revision": "f161713ab57f518fe7837bd7a4e28195"
   },
   {
-    "url": "/_nuxt/pages/about.02a553ca53bb7bd48979.js",
-    "revision": "f4b1c004de477e2fe5fce04763e50699"
+    "url": "/_nuxt/pages/about.e33c85093c4da475f5c4.js",
+    "revision": "a414c5817b26c16c09689c8e28ae8bbc"
   },
   {
-    "url": "/_nuxt/pages/admin.a56af347eeae272db047.js",
-    "revision": "dfaebcc39b857a6cd823cc50e73b0670"
+    "url": "/_nuxt/pages/admin.c06203c44bed1fa6fe77.js",
+    "revision": "e98b6aff55e932c1f9c6cf5773c9e873"
   },
   {
-    "url": "/_nuxt/pages/admin/index.c5b3a4fae681a1973ded.js",
-    "revision": "61e2e94d850f8620ee63d68d5c810778"
+    "url": "/_nuxt/pages/admin/index.3965fdba414ff4534985.js",
+    "revision": "28a83d31f63069ffe8e91c97724fa02b"
   },
   {
-    "url": "/_nuxt/pages/admin/items.f8a0e89e2862afd811cf.js",
-    "revision": "e223a40376b22bbaee5771a864453114"
+    "url": "/_nuxt/pages/admin/items.57cc33524c0ca3a4bac9.js",
+    "revision": "8b1cce81c568d5c6ab13e48de171d3c1"
   },
   {
     "url": "/_nuxt/pages/admin/settings.fc19131d4f8a344d7767.js",
     "revision": "bc780db7be66559e5e758e7806bd8278"
   },
   {
-    "url": "/_nuxt/pages/index.84f949f16af3743ef543.js",
-    "revision": "e4fd760ea9e9412a27c4c08e35813922"
+    "url": "/_nuxt/pages/index.e908b3ee4e435207ee9e.js",
+    "revision": "7a6fdd187f18f9061ddf2078fd1a23d4"
   },
   {
-    "url": "/_nuxt/pages/login.ad18eeac39f38d4f4b7f.js",
-    "revision": "d0592c9b89fb30c73464a5dd7da140ab"
+    "url": "/_nuxt/pages/login.ff9d66083f90ac7a5ea5.js",
+    "revision": "1415dc0d7cd8198304b16dbe3b84a29e"
   },
   {
-    "url": "/_nuxt/vendor.120fbac831a9cfb8eea1.js",
-    "revision": "53242636d08ed243c78c0510aa5dc146"
+    "url": "/_nuxt/vendor.5282f7ed0b9c3cedac78.js",
+    "revision": "3b01f2de20732d71a93b5dbd77e3e933"
   }
-])
+], {
+  "cacheId": "nuxt-firebase-auth",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
 
 
-workboxSW.router.registerRoute(new RegExp('/_nuxt/.*'), workboxSW.strategies.cacheFirst({}), 'GET')
 
-workboxSW.router.registerRoute(new RegExp('/.*'), workboxSW.strategies.networkFirst({}), 'GET')
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+
+workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
+
+
+
+
 
